43ab60cd703ee10658d25b68b90375cdde753a9a
ODSEFIANI <92064461+ODSEFIANI@users.noreply.github.com>
2024-01-11 13:49:29 +0100

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,18 +1,41 @@
 #!/usr/bin/python3
-"""lockboxes Interview new Challenge"""
+from collections import Counter
 
 
 def canUnlockAll(boxes):
+    list1 = list(range(0, len(boxes)))
+    print(list1)
+    list2 = []
+    locked = []
+    match = []
+    duplicates = []
 
-    """lockboxes Interview new Challenge"""
-    def dfs(box_index):
-        visited.add(box_index)
-        for key in boxes[box_index]:
-            if key not in visited:
-                dfs(key)
+    for index, box in enumerate(boxes):
+        for value in box:
+            list2.append(value)
 
-    n = len(boxes)
-    visited = set()
-    dfs(0)
+            # Check if the value is equal to the box number
+            if value == index:
+                match.append(value)
 
-    return len(visited) == n
+    # Count occurrences of each value in list2
+    counter = Counter(list2)
+
+    # Find duplicates in list2
+    duplicates = [item for item, count in counter.items() if count > 1]
+
+    # Check if all elements in match are also duplicates
+    for value in match:
+        if value not in duplicates:
+            return False
+
+    # Check for locked boxes
+    for elem in list2:
+        if elem not in list1:
+            locked.append(elem)
+
+    # Check if all boxes can be opened
+    if len(locked) == 0:
+        return True
+    else:
+        return False
