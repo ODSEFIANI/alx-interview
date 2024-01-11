4f9165d5e920cbc88a591fe2ae5b5deb627625e4
ODSEFIANI <92064461+ODSEFIANI@users.noreply.github.com>
2024-01-11 14:08:01 +0100

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,41 +1,20 @@
 #!/usr/bin/python3
-from collections import Counter
-
-
 def canUnlockAll(boxes):
-    list1 = list(range(0, len(boxes)))
-    print(list1)
-    list2 = []
-    locked = []
-    match = []
-    duplicates = []
 
-    for index, box in enumerate(boxes):
-        for value in box:
-            list2.append(value)
+    # Set to keep track of unlocked boxes
+    unlocked_boxes = set([0])
 
-            # Check if the value is equal to the box number
-            if value == index:
-                match.append(value)
+    # Function to perform depth-first search
+    def dfs(box_index):
 
-    # Count occurrences of each value in list2
-    counter = Counter(list2)
+        for key in boxes[box_index]:
+            if key not in unlocked_boxes:
+                unlocked_boxes.add(key)
+                dfs(key)
 
-    # Find duplicates in list2
-    duplicates = [item for item, count in counter.items() if count > 1]
+    # Start DFS from the first box
+    dfs(0)
 
-    # Check if all elements in match are also duplicates
-    for value in match:
-        if value not in duplicates:
-            return False
-
-    # Check for locked boxes
-    for elem in list2:
-        if elem not in list1:
-            locked.append(elem)
-
-    # Check if all boxes can be opened
-    if len(locked) == 0:
-        return True
-    else:
-        return False
+    # Check if all boxes are unlocked
+    return len(unlocked_boxes) == len(boxes)
+    
