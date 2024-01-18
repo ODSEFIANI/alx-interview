dbb27211b3c9f7052d4aba7bb786afbef732472b
ODSEFIANI <oussma.sefiani@um5r.ac.ma>
2024-01-18 18:55:56 +0100

    0. Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,27 +1,38 @@
 #!/usr/bin/python3
-'''py module.
-'''
+'''Minimum Operations python3 challenge'''
 
 
 def min_operations(n):
-    '''optmize the calcylation
-    and n operation.
-    Returns:
-        Integer : number of opration
-    '''
-    if n <= 1:
+    """optimztion
+    """
+    current_chars = 1
+    clibBd = 0
+    operations_counter = 0
+
+    while current_chars < n:
+        if clibBd == 0:
+            clibBd = current_chars
+            operations_counter += 1
+
+        if current_chars == 1:
+            current_chars += clibBd
+            operations_counter += 1
+            continue
+
+        remaining_chars = n - current_chars
+
+        if remaining_chars < clibBd:
+            return 0
+
+        if remaining_chars % current_chars != 0:
+            current_chars += clibBd
+            operations_counter += 1
+        else:
+            clibBd = current_chars
+            current_chars += clibBd
+            operations_counter += 2
+
+    if current_chars == n:
+        return operations_counter
+    else:
         return 0
-
-    min_ops = 0
-    current_len = 1
-    clipboard = 0
-
-    while current_len < n:
-        if n % current_len == 0:
-            clipboard = current_len
-            min_ops += 1
-
-        current_len += clipboard
-        min_ops += 1
-
-    return min_ops
