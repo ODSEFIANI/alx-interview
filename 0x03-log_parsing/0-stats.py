dec346be2e3edf258cdf2113884b138ad81eb7a2
ODSEFIANI <oussma.sefiani@um5r.ac.ma>
2024-02-02 13:30:38 +0100

    logparsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
-"""
-A script that reads stdin line by line and computes metrics
-"""
 
 import sys
 
 
-def print_statistics(status_counts, total_size):
+def print_msg(dc, fileSize):
     """
-    Print statistics based on status counts and total size.
+    Method to print
     """
-    print("Total file size: {}".format(total_size))
-    for status_code, count in sorted(status_counts.items()):
-        if count != 0:
-            print("{}: {}".format(status_code, count))
+
+    print("File size: {}".format(fileSize))
+    for k, v in sorted(dc.items()):
+        if v != 0:
+            print("{}: {}".format(k, v))
 
 
-status_counts = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
-
-total_size = 0
-line_count = 0
+fileSize = 0
+code = 0
+count = 0
+dc = {"200": 0,
+           "301": 0,
+           "400": 0,
+           "401": 0,
+           "403": 0,
+           "404": 0,
+           "405": 0,
+           "500": 0}
 
 try:
     for line in sys.stdin:
@@ -36,18 +32,18 @@ try:
         parsed_line = parsed_line[::-1]
 
         if len(parsed_line) > 2:
-            line_count += 1
+            count += 1
 
-            if line_count <= 10:
-                total_size += int(parsed_line[0])
-                status_code = parsed_line[1]
+            if count <= 10:
+                fileSize += int(parsed_line[0])
+                code = parsed_line[1]
 
-                if status_code in status_counts:
-                    status_counts[status_code] += 1
+                if (code in dc.keys()):
+                    dc[code] += 1
 
-            if line_count == 10:
-                print_statistics(status_counts, total_size)
-                line_count = 0
+            if (count == 10):
+                print_msg(dc, fileSize)
+                count = 0
 
 finally:
-    print_statistics(status_counts, total_size)
+    print_msg(dc, fileSize)
