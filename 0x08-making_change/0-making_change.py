cb28f9e82620e765bca396facfce9d13ff893be3
ODSEFIANI <oussma.sefiani@um5r.ac.ma>
2024-02-29 23:24:53 +0100

    fix indentation

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,23 +4,19 @@ py module, dynamic programming
 '''
 import sys
 
+
 def makeChange(coins, total):
     '''
     Return: Fewest number of coins needed to meet the total amount.
-    
+
     If the total amount is 0 or less, return 0.
-    
-    If the total amount cannot be met using any combination of the given coins, return -1.
-    
+
+    If the total amount cannot be met using any combination of
+    the given coins, return -1.
+
     Parameters:
     - coins (list): A list of coin values.
     - total (int): The target amount.
-
-    Example:
-    >>> coins = [1, 2, 5]
-    >>> total_amount = 11
-    >>> result = minCoins(coins, total_amount)
-    >>> print(f"The fewest number of coins needed to make {total_amount} is: {result}")
     '''
     if total <= 0:
         return 0
@@ -31,7 +27,8 @@ def makeChange(coins, total):
         for coin_index in range(coin_count):
             if coins[coin_index] <= amount:
                 sub_result = dp_table[amount - coins[coin_index]]
-                if sub_result != sys.maxsize and sub_result + 1 < dp_table[amount]:
+                if sub_result != sys.maxsize and \
+                        sub_result + 1 < dp_table[amount]:
                     dp_table[amount] = sub_result + 1
 
     if dp_table[total] == sys.maxsize:
